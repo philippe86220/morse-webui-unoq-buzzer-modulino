0cc528b6f787675b7a43f9d8675c379a8ed4deee
philippe86220 <142721115+philippe86220@users.noreply.github.com>
2026-01-30 15:52:51 +0100

    Add Morse class for generating Morse code sounds

--- sketch/Morse.h
+++ sketch/Morse.h
@@ -1,1 +1,26 @@
+#ifndef MORSE_H
+#define MORSE_H
+
+#include <Arduino.h>
+#include "Modulino.h"
+
+class Morse {
+  public:
+    Morse(ModulinoBuzzer& buzzer, char* s, int vitesse);
+    void joue(void);
+    ~Morse();
+
+  private:
+    ModulinoBuzzer& _buzzer;
+    char* _s;
+    int _vitesse;
+
+    static const int NOTE_C4 = 262;
+
+    void point(int duree, int frequence);
+    void tiret(int duree, int frequence);
+    void caractere(int vitesse, int frequence, int index);
+};
+
+#endif
 
